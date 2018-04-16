3deb6408a3fa73866ac1168864cdae9f65ca1952
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2018-04-15 21:59:30 -0400

    Add code to playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -4,3 +4,21 @@ playList.push('Pierwsza paly lista');
 playList.push('Respect', 'Imagine');
 playList.unshift('Born');
 printList(playList);
+
+
+var nums = [1, 2, 3, 4, 5];
+
+console.log(nums.length);
+
+nums.push(6, 7);
+
+printList(nums);
+
+nums.pop();
+nums.unshift(0);
+nums.shift();
+nums.shift();
+nums.shift();
+
+
+printList(nums);
